ff0bfaccef73d71f10f48227a1151e5def9159c5
ShrianshGoel <76607815+ShrianshGoel@users.noreply.github.com>
2024-10-19 21:13:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("#ne").addEventListener('click',function(){
+document.querySelector(".de1").style.left="57%";
+
+});
+document.querySelector("#dse").addEventListener("click",function(){
+    alert("Payment Confirmed");
+});
